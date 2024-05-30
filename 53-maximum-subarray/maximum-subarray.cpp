e2fac97921b2a135df6ffc6b1717d8bee439298e
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-05-30 13:25:53 +0530

    Time: 72 ms (78.74%) | Memory: 72.8 MB (5.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,18 +1,14 @@
-#include <vector>
-#include <algorithm>
 
 class Solution {
 public:
-    int maxSubArray(std::vector<int>& nums) {
-        std::vector<int> dp(nums.size());
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size());
         dp[0] = nums[0];
-        int max_sum = dp[0]; // Initialize max_sum with the first element
+        int max_sum = dp[0];
 
         for(int i = 1; i < nums.size(); i++) {
-            // If adding nums[i] to the previous sum results in a larger sum, update the sum
-            dp[i] = std::max(nums[i], nums[i] + dp[i - 1]);
-            // Update max_sum if necessary
-            max_sum = std::max(max_sum, dp[i]);
+            dp[i] = max(nums[i], nums[i] + dp[i - 1]);
+            max_sum = max(max_sum, dp[i]);
         }
         
         return max_sum;
