4527c5bcc8bb1f7360419f4936b2ab547bfc740c
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-05-30 09:34:44 +0530

    Time: 12 ms (38.51%) | Memory: 16.5 MB (25.11%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -6,7 +6,6 @@ public:
         bool firstRowHasZero = false;
         bool firstColHasZero = false;
 
-        // Check if the first column has any zeroes
         for (int i = 0; i < rows; i++) {
             if (matrix[i][0] == 0) {
                 firstColHasZero = true;
@@ -14,7 +13,6 @@ public:
             }
         }
 
-        // Check if the first row has any zeroes
         for (int j = 0; j < cols; j++) {
             if (matrix[0][j] == 0) {
                 firstRowHasZero = true;
@@ -22,7 +20,6 @@ public:
             }
         }
 
-        // Use first row and column to mark zeroes in the rest of the matrix
         for (int i = 1; i < rows; i++) {
             for (int j = 1; j < cols; j++) {
                 if (matrix[i][j] == 0) {
@@ -32,7 +29,6 @@ public:
             }
         }
 
-        // Set matrix cells to zero based on the markers in the first row and column
         for (int i = 1; i < rows; i++) {
             for (int j = 1; j < cols; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -41,14 +37,12 @@ public:
             }
         }
 
-        // Zero out the first row if needed
         if (firstRowHasZero) {
             for (int j = 0; j < cols; j++) {
                 matrix[0][j] = 0;
             }
         }
 
-        // Zero out the first column if needed
         if (firstColHasZero) {
             for (int i = 0; i < rows; i++) {
                 matrix[i][0] = 0;
