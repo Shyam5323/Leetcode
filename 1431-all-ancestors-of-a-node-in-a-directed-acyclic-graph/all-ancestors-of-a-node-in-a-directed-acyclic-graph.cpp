7373fe4b4f3938b2229a726d12ebbc09698389f6
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-06-29 12:03:36 +0530

    Time: 354 ms (36.33%) | Memory: 159.9 MB (13.72%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    unordered_map<int, vector<int>> adj;
+    void iterativeDFS(int start, set<int>& ancestors) {
+        stack<int> st;
+        st.push(start);
+        while (!st.empty()) {
+            int node = st.top();
+            st.pop();
+            for (int parent : adj[node]) {
+                if (ancestors.insert(parent).second) {
+                    st.push(parent);
+                }
+            }
+        }
+    }
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> ans(n);
+        for (auto& edge : edges) {
+            adj[edge[1]].push_back(edge[0]);
+        }
+        for (int i = 0; i < n; ++i) {
+            set<int> ancestors;
+            iterativeDFS(i, ancestors);
+            ans[i] = vector<int>(ancestors.begin(), ancestors.end());
+        }      
+        return ans;
+    }
+};
