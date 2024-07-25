830e39e07d0cc80da512d080ed7d4f6009d3870f
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-07-25 13:26:48 +0530

    Time: 75 ms (95.39%) | Memory: 74.2 MB (55.37%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> sortArray(std::vector<int>& nums) {
+        int maxE = *max_element(nums.begin(), nums.end());
+        int minE = *min_element(nums.begin(), nums.end());
+
+        std::vector<int> arr(maxE - minE + 1);
+        for(int i : nums) {
+            arr[i - minE]++;
+        }
+        
+        std::vector<int> ans;
+        for(int i = 0; i < arr.size(); i++) {
+            while(arr[i] > 0) {
+                ans.push_back(i + minE); 
+                arr[i]--;
+            }
+        }
+        return ans;
+    }
+};
