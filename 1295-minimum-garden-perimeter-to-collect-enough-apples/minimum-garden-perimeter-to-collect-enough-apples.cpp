37008d3812dc25265f9379d173b355eb89817903
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-08-14 10:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (14.72%) - LeetSync

--- 1295-minimum-garden-perimeter-to-collect-enough-apples/minimum-garden-perimeter-to-collect-enough-apples.cpp
+++ 1295-minimum-garden-perimeter-to-collect-enough-apples/minimum-garden-perimeter-to-collect-enough-apples.cpp
@@ -1,13 +1,28 @@
 class Solution {
+private:    
+    static long long TotalApples(long long n) {
+        long long n_sq = n*n;
+        return 4*n_sq*(n+1)+2*n_sq+2*n;
+    }
+
 public:
-    long long minimumPerimeter(long long n) {
-        long long index = 0;
-        for (long long i = 1;; ++i) {
-            if (n <= 2 * i * (i + 1) * (2 * i + 1)) {
-                index = i;
-                break;
+    long long minimumPerimeter(long long neededApples) {
+
+        long long low_n = 1;
+        long long high_n = 100000; 
+
+        while (low_n < high_n) {
+            long long mid_n = low_n + (high_n-low_n) / 2;
+            long long apples = TotalApples(mid_n);
+            if (apples == neededApples) {
+                return 8*mid_n;
+            } else if (apples < neededApples) {
+                low_n = mid_n+1;
+            } else {
+                high_n = mid_n;
             }
         }
-        return index * 8;
+
+        return 8*high_n;
     }
 };
