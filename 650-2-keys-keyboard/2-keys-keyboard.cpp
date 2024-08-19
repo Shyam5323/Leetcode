d862baca1be0b63587e4faf194609e50d8509a5e
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-08-19 10:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (71.95%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,35 +1,15 @@
-
 class Solution {
 public:
-    long long recurse(int current, int copied, int target ) {
-        if (current == target) {
-            return 0; 
-        }
-        if (current > target) {
-            return INT_MAX; 
-        }
-        // if(dp[current] != -1) {
-        //     return dp[current];
-        // }
-
-        long long noCopy = INT_MAX;
-        long long copy = INT_MAX;
-
-
-        if (copied != 0) {
-            noCopy = 1 + recurse(current + copied, copied, target);
-        }
-        
-        copy = 2 + recurse(2 * current, current, target);
-
-        return min(copy, noCopy);
-    }
-
-    int minSteps(int n) {
-        if (n == 1) {
-            return 0; 
-        }
-        vector<int> dp(n+1, -1);
-        return recurse(1, 0, n ); 
-    }
+   int minSteps(int n) {
+       int ans = 0, sqroot = sqrt(n);
+       for(int d = 2; d <= sqroot; d++) {
+           while(n % d == 0) {
+               ans += d;
+               n /= d;
+           }
+       }
+       if(n > 1)
+           ans += n;
+       return ans;
+   }
 };
