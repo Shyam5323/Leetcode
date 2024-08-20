3336667a22acd187e626f292291f20abdad1db1d
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-08-20 16:04:15 +0530

    Time: 27 ms (56.06%) | Memory: 15.8 MB (53.47%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    void print(vector<int>& a) {
-        for(int i : a) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
     
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<long long>> dist(n, vector<long long>(n, INT_MAX));
         
-        // Initialize distances
         for(auto& v : edges) {
             dist[v[0]][v[1]] = v[2];
-            dist[v[1]][v[0]] = v[2];  // Undirected graph
+            dist[v[1]][v[0]] = v[2];  
         }
         for(int i = 0; i < n; i++) {
             dist[i][i] = 0;
         }
         
-        // Floyd-Warshall algorithm
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
