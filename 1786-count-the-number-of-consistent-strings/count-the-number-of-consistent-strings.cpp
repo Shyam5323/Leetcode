4da0f1b21076133a97f499fbeebb9c614d525b2a
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-09-12 16:16:00 +0530

    Time: 34 ms (96.02%) | Memory: 34 MB (58.62%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
+        ios_base::sync_with_stdio(false);
         unordered_set<char> c;
         for(char ch : allowed) {
             c.insert(ch);
