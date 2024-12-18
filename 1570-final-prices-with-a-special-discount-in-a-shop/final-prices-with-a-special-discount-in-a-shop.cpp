1b3556002ce27ad124b36fda2c11dba90cb86397
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-12-18 11:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (24.39%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
-        vector<int> res;
         for(int i=0; i<n; i++) {
             int discount = 0;
             for(int j = i+1; j<n; j++) {
@@ -11,9 +10,9 @@ public:
                     break;
                 }
             }
-            res.push_back(prices[i] - discount);
+            prices[i] = prices[i] - discount;
         }
-        return res;
+        return prices;
 
     }
 };
