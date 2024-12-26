90eca7a0238f17068456e6b7a94baba2f2d7347a
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-12-26 22:06:31 +0530

    Time: 12 ms (62.53%) | Memory: 25.7 MB (27.12%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    void recurse(vector<int>& nums, int target, int index, int& count, int sum) {
-        if(index >= nums.size() ) {
-            if(sum == target) {
-                count++;
-            }
-            return;
-        }
-        recurse(nums, target, index+ 1, count, sum + nums[index]);
-        recurse(nums, target, index+ 1, count, sum - nums[index]);
-    }
+    int totalSum;
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int count = 0;
-        recurse(nums, target, 0, count, 0);
-        return count;
+        totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<vector<int>> memo(
+            nums.size(),
+            vector<int>(2 * totalSum + 1, INT_MIN));
+        return calculateWays(nums, 0, 0, target, memo);
+    }
+
+    int calculateWays(vector<int>& nums, int currentIndex, int currentSum,
+                      int target, vector<vector<int>>& memo) {
+        if (currentIndex == nums.size()) {
+            if (currentSum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[currentIndex][currentSum + totalSum] !=
+                INT_MIN) {
+                return memo[currentIndex][currentSum + totalSum];
+            }
+            int add =
+                calculateWays(nums, currentIndex + 1,
+                              currentSum + nums[currentIndex], target, memo);
+
+            int subtract =
+                calculateWays(nums, currentIndex + 1,
+                              currentSum - nums[currentIndex], target, memo);
+
+            memo[currentIndex][currentSum + totalSum] = add + subtract;
+
+            return memo[currentIndex][currentSum + totalSum];
+        }
     }
 };
