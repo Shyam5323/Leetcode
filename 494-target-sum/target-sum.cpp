84883000ab014b429d41e3566d7850330c7d7c31
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2024-12-26 22:06:58 +0530

    Time: 19 ms (54.77%) | Memory: 25.8 MB (26.42%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -20,17 +20,12 @@ public:
                 return 0;
             }
         } else {
-            if (memo[currentIndex][currentSum + totalSum] !=
-                INT_MIN) {
+            if (memo[currentIndex][currentSum + totalSum] != INT_MIN) {
                 return memo[currentIndex][currentSum + totalSum];
             }
-            int add =
-                calculateWays(nums, currentIndex + 1,
-                              currentSum + nums[currentIndex], target, memo);
+            int add = calculateWays(nums, currentIndex + 1, currentSum + nums[currentIndex], target, memo);
 
-            int subtract =
-                calculateWays(nums, currentIndex + 1,
-                              currentSum - nums[currentIndex], target, memo);
+            int subtract = calculateWays(nums, currentIndex + 1, currentSum - nums[currentIndex], target, memo);
 
             memo[currentIndex][currentSum + totalSum] = add + subtract;
 
