aaa54925af97e859874c4902bb582fc6631f0b1d
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2025-01-08 10:24:55 +0530

    Time: 8 ms (24.15%) | Memory: 45.7 MB (5.08%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,16 +1,54 @@
+class Node {
+public:
+    Node* links[26] = {};
+    bool contains(char c) { return links[c - 'a'] != NULL; }
+    void put(char c, Node* node) { links[c - 'a'] = node; }
+    Node* next(char c) { return links[c - 'a']; }
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() { root = new Node(); }
+    void insert(string& word) {
+        Node* node = root;
+        for (char c : word) {
+            if (!node->contains(c)) {
+                node->put(c, new Node());
+            }
+            node = node->next(c);
+        }
+    }
+    bool startsWith(string& prefix) {
+        Node* node = root;
+        for (char c : prefix) {
+            if (!node->contains(c)) {
+                return false;
+            }
+            node = node->next(c);
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
-    bool isPrefixAndSuffix(string str1, string str2) {
-        if((str2.substr(0, str1.length()) == str2.substr(str2.length() - str1.length() , str1.length())) && (str1 == str2.substr(0, str1.length()))) {
-            return true;
-        }
-        return false;
-    }
     int countPrefixSuffixPairs(vector<string>& words) {
+        int n = words.size();
         int count = 0;
-        for(int i = 0; i<words.size(); i++) {
-            for(int j = i+1; j<words.size(); j++) {
-                if(i != j && words[i].size() <= words[j].size() && isPrefixAndSuffix(words[i], words[j])) {
+        for (int i = 0; i < n; i++) {
+            Trie prefixTrie, suffixTrie;
+            prefixTrie.insert(words[i]);
+            string revWord = words[i];
+            reverse(revWord.begin(), revWord.end());
+            suffixTrie.insert(revWord);
+            for (int j = 0; j < i; j++) {
+                if (words[j].size() > words[i].size()) continue;
+                string prefixWord = words[j];
+                string revPrefixWord = prefixWord;
+                reverse(revPrefixWord.begin(), revPrefixWord.end());
+                if (prefixTrie.startsWith(prefixWord) &&
+                    suffixTrie.startsWith(revPrefixWord)) {
                     count++;
                 }
             }
