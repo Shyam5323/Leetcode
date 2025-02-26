e51fef1284692e7c31cce2469c89c464d217f86d
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2025-02-26 12:19:42 +0530

    Time: 2 ms (27.80%) | Memory: 48.3 MB (6.06%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -5,16 +5,17 @@ public:
         vector<pair<int,int>> dp(nums.size() + 1);
         dp[0].first = nums[0];
         dp[0].second = nums[0];
+        int ans = 0;
+        ans = max(ans, abs(dp[0].first));
+        ans = max(ans, abs(dp[0].second));
         for(int i = 1; i<nums.size(); i++) {
             dp[i].first = max(nums[i], nums[i] + dp[i-1].first);
             dp[i].second = min(nums[i], nums[i] + dp[i-1].second);
+            ans = max(ans, abs(dp[i].first));
+            ans = max(ans, abs(dp[i].second));
         }
-        int ans = 0;
-        for(auto p : dp) {
-            auto [f, s] = p;
-            ans = max(ans, abs(f));
-            ans = max(ans, abs(s));
-        }
+   
+    
         return ans;
     }
 };
