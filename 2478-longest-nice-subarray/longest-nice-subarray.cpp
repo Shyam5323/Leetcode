1af868c267cbe8e6360783c86010c2e4e3403c11
Chandan Singh Rathore <121493988+Shyam5323@users.noreply.github.com>
2025-03-19 02:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 61.1 MB (28.27%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        vector<int> freq(32, 0);
+        int left = 0, right = 0, andVal = 0, maxLen = 0;
+
+        while (right < nums.size()) {
+            while ((andVal & nums[right]) != 0) {
+                andVal ^= nums[left]; 
+                left++;
+            }
+            andVal |= nums[right]; 
+            maxLen = max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
+    }
+};
